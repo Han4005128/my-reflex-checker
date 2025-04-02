04b3f39c728c1198b4c8a25735f455b940c6383b
Han4005128 <gksehddnr223@gmail.com>
2025-04-02 13:37:11 +0900

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,1 @@
-import ReflexChecker from "../components/ReflexChecker";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <img src="/logo.png" alt="스포츠토토 로고" className="w-48 mb-6" />
-      <ReflexChecker />
-    </main>
-  );
-}
+// placeholder for page.tsx
